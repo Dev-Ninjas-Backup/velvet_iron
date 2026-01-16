f7b28ae615cd1117899030597108d99bf26d06bf
shahria <shahriasayad9@gmail.com>
2026-01-17 03:33:42 +0600

    adding onboarding done

--- lib/features/onboarding6/screen/onboarding6_screen.dart
+++ lib/features/onboarding6/screen/onboarding6_screen.dart
@@ -4,16 +4,16 @@ import 'package:velvet_iron/core/common/widgets/custom_back_button.dart';
 import 'package:velvet_iron/core/common/widgets/custom_background_withimage.dart';
 import 'package:velvet_iron/core/common/widgets/custom_button.dart';
 import 'package:velvet_iron/core/utils/constants/image_path.dart';
-import 'package:velvet_iron/features/onboarding5/controller/onboarding5_controller.dart';
-import 'package:velvet_iron/features/onboarding5/widgets/onboarding5_widget.dart';
+import 'package:velvet_iron/features/onboarding6/controller/onboarding6_controller.dart';
+import 'package:velvet_iron/features/onboarding6/widgets/onboarding6_widget.dart';
 import 'package:velvet_iron/routes/app_routes.dart';
 
-class OnboardingScreen5 extends StatelessWidget {
-  const OnboardingScreen5({super.key});
+class OnboardingScreen6 extends StatelessWidget {
+  const OnboardingScreen6({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Get.put(OnboardingController5());
+    Get.put(OnboardingController6());
 
     return Scaffold(
       body: CustomBackgroundWithImage(
@@ -26,28 +26,22 @@ class OnboardingScreen5 extends StatelessWidget {
                 child: Column(
                   children: [
                     const SizedBox(height: 60),
-                    const StepsTextWidget5(),
-                    const SizedBox(height: 16),
-                    const ProgressBarWidget5(),
-                    const SizedBox(height: 32),
-                    const OnboardingHeader5Widget(),
-                    const SizedBox(height: 32),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 24),
-                      child: DateSelectionWidget(),
-                    ),
+                    const StepsTextWidget6(),
+                    const SizedBox(height: 10),
+                    const ProgressBarWidget6(),
+                    const SizedBox(height: 40),
+                    const OnboardingHeaderWidget6(),
+                    const SizedBox(height: 40),
+                    const WeightSelectionWidget(),
                     const SizedBox(height: 20),
-
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 24),
                       child: CustomButton(
                         label: 'Continue (+10 XP)',
                         onPressed: () =>
-                            Get.toNamed(AppRoute.getonboardingScreen6()),
+                            Get.toNamed(AppRoute.getonboardingScreen7()),
                       ),
                     ),
-
-                    const SizedBox(height: 24),
                   ],
                 ),
               ),
