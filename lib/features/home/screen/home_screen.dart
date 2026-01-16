90f3b31342674111fe9daaa02ae611623a3e2510
shahria <shahriasayad9@gmail.com>
2026-01-17 05:41:30 +0600

    graph added

--- lib/features/home/screen/home_screen.dart
+++ lib/features/home/screen/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:velvet_iron/core/utils/constants/colors.dart';
 import 'package:velvet_iron/features/bottom_nav/controller/bottom_nav_controller.dart';
-import 'package:velvet_iron/features/exercise/screen/exercise_screen.dart';
 import 'package:velvet_iron/features/home/widgets/mood_selector.dart';
 import 'package:velvet_iron/features/home/widgets/todo_list.dart';
 import 'package:velvet_iron/features/daily_logs/screen/daily_log_screen.dart';
@@ -53,7 +52,7 @@ class HomeScreen extends StatelessWidget {
                   _buildHomeContent(),
                   const DailyLogScreen(),
                   MedicationScreen(),
-                  const ExerciseScreen(),
+                  // const ExerciseScreen(),
                   const Center(
                     child: Text(
                       "Exercise",
--- lib/features/home/widgets/weight_progress.dart
+++ lib/features/home/widgets/weight_progress.dart
@@ -1,8 +1,6 @@
+import 'dart:math';
 
-
-
-
-
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:velvet_iron/core/common/styles/global_text_style.dart';
 import 'package:velvet_iron/core/utils/constants/colors.dart';
@@ -12,13 +10,19 @@ class WeightProgress extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Random random = Random();
+    final List<double> weeklyData = List.generate(
+      7,
+      (index) => 20 + random.nextDouble() * 80,
+    );
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-             Text(
+            Text(
               "Weight Progress (kg)",
               style: getTextStyle(color: Colors.white, fontSize: 18),
             ),
@@ -26,44 +30,53 @@ class WeightProgress extends StatelessWidget {
               padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
               decoration: BoxDecoration(
                 border: Border.all(color: AppColors.textFieldBorderColor),
-                color:Colors.transparent,
+                color: Colors.transparent,
                 borderRadius: BorderRadius.circular(15),
               ),
               child: Row(
-                children:  [
+                children: [
                   Text(
                     "This week",
-                    style: getTextStyle(color:Colors.white, fontSize: 12),),
-                
-                  SizedBox(width: 4),
-                  Icon(
-                    Icons.arrow_drop_down,
-                    color: Colors.white,
+                    style: getTextStyle(color: Colors.white, fontSize: 12),
                   ),
+                  const SizedBox(width: 4),
+                  const Icon(Icons.arrow_drop_down, color: Colors.white),
                 ],
               ),
             ),
           ],
         ),
         const SizedBox(height: 12),
-        Container(
+        SizedBox(
           height: 150,
-          padding: const EdgeInsets.all(16),
-          decoration: BoxDecoration(
-
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: List.generate(
-              7,
-              (index) => Container(
-                width: 18,
-                decoration: BoxDecoration(
-                  color: AppColors.gold,
-                  borderRadius: BorderRadius.circular(6),
-                ),
-              ),
+          child: BarChart(
+            BarChartData(
+              alignment: BarChartAlignment.spaceBetween,
+              barTouchData: BarTouchData(enabled: false),
+              titlesData: const FlTitlesData(show: false),
+              gridData: const FlGridData(show: false),
+              borderData: FlBorderData(show: false),
+              maxY: 100,
+              barGroups: weeklyData.asMap().entries.map((e) {
+                return BarChartGroupData(
+                  x: e.key,
+                  barRods: [
+                    BarChartRodData(
+                      toY: e.value,
+                      width: 29,
+                      gradient: const LinearGradient(
+                        colors: [
+                          Color(0xFF683E23), // bottom
+                          Color(0xFFE9B86E), // top
+                        ],
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                      ),
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                  ],
+                );
+              }).toList(),
             ),
           ),
         ),
