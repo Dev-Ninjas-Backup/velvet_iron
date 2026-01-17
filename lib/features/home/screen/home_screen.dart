30a64d6a476af2f42c6aae116483301798a8dab9
shahria <shahriasayad9@gmail.com>
2026-01-18 02:12:41 +0600

    home screen error sixed

--- lib/features/home/screen/home_screen.dart
+++ lib/features/home/screen/home_screen.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:velvet_iron/core/utils/constants/colors.dart';
 import 'package:velvet_iron/features/bottom_nav/controller/bottom_nav_controller.dart';
+import 'package:velvet_iron/features/daily_logs/screen/daily_log_screen.dart';
 import 'package:velvet_iron/features/home/widgets/mood_selector.dart';
 import 'package:velvet_iron/features/home/widgets/todo_list.dart';
+import 'package:velvet_iron/features/medication_screen/screen/medication_screen.dart';
 import '../widgets/header_section.dart';
 import '../widgets/welcome_card.dart';
 import '../widgets/weight_progress.dart';
@@ -45,17 +47,14 @@ class HomeScreen extends StatelessWidget {
             final controller = Get.find<BottomNavController>();
             return Padding(
               padding: const EdgeInsets.only(bottom: 115),
-              child: controller.getCurrentScreen(),
-            );
-          }),
               child: IndexedStack(
                 index: controller.tabIndex.value,
                 children: [
-                  _buildHomeContent(),
+                  const HomeScreenContent(),
                   const DailyLogScreen(),
-                  MedicationScreen(),
+                  const MedicationScreen(),
+
                   // const ExerciseScreen(),
-                  
                   const Center(
                     child: Text(
                       "Exercise",
@@ -76,8 +75,8 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ],
               ),
-            ),
-          ),
+            );
+          }),
           const Positioned(bottom: 20, left: 0, right: 0, child: BottomNav()),
         ],
       ),
