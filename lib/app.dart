1b5c291496eb7f2d95a5805993b60102428a5349
shahria <shahriasayad9@gmail.com>
2026-01-24 01:21:02 +0600

    files added

--- lib/app.dart
+++ lib/app.dart
@@ -19,7 +19,7 @@ class VelvetIron extends StatelessWidget {
         return GetMaterialApp(
           debugShowCheckedModeBanner: false,
           translations: AppTranslations(),
-          initialRoute: AppRoute.getthemeOnboardingScreen(),
+          initialRoute: AppRoute.getSplashScreen(),
           getPages: AppRoute.routes,
           initialBinding: ControllerBinder(),
           themeMode: ThemeMode.system,
