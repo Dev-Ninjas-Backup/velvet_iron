f04e5dd4b32d019e7ff1da2a6504332297703c3d
imran <mr.t.imran11@gmail.com>
2026-01-31 06:30:56 +0600

    update password screen updated

--- lib/features/update_password/widgets/update_password_form.dart
+++ lib/features/update_password/widgets/update_password_form.dart
@@ -10,7 +10,6 @@ class UpdatePasswordFormWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(UpdatePasswordController());
-
     return Container(
       padding: const EdgeInsets.all(20),
       decoration: BoxDecoration(
